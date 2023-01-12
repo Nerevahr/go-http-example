38c856540a32ced2d803f9e2812f417afc4582c7
Mathieu Verpillat <mathieu.verpillat@hiit-consulting.fr>
2023-01-12 17:07:50 +0000

    test http api

--- custom/custom.go
+++ custom/custom.go
@@ -1,14 +1,14 @@
-package custom
+// package custom
 
-import (
-	"net/http"
-)
+// import (
+// 	"net/http"
+// )
 
-func Hello(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Hello, world!"))
-}
+// func Hello(w http.ResponseWriter, req *http.Request) {
+// 	w.Write([]byte("Hello, world!"))
+// }
 
-func Display() {
-	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":8090", nil)
-}
+// func Display() {
+// 	http.HandleFunc("/", Hello)
+// 	http.ListenAndServe(":8090", nil)
+// }
--- main.go
+++ main.go
@@ -1,9 +1,62 @@
 package main
 
 import (
-	. "github.com/Nerevahr/go-http-example/custom"
+    "github.com/gin-gonic/gin"
+    "github.com/jinzhu/gorm"
+	"net/http"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
+	"fmt"
+	"log"
 )
 
+type Module struct {
+    ID      string `json:"id"`
+    Name    string `json:"name"`
+    Courses []Course
+}
+
+type Course struct {
+    ID   string `json:"id"`
+    Name string `json:"name"`
+}
+
 func main() {
-	Display()
+	username := "postgres"
+	password := "password"
+	host := "localhost"
+	dbname := "test"
+	
+	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", host, username, dbname, password))
+	
+	if err != nil {
+		log.Fatal("Could not connect to the database: ", err)
+	}
+	defer db.Close()
+	
+    db.AutoMigrate(&Module{}, &Course{})
+
+	r := gin.Default()
+	r.POST("/modules", createModule(db))	
+    r.GET("/modules", getModules(db))
+    r.Run()
+}
+
+func getModules(db *gorm.DB) gin.HandlerFunc {
+    return func(c *gin.Context) {
+        var modules []Module
+        db.Preload("Courses").Find(&modules)
+        c.JSON(200, modules)
+    }
+}
+
+func createModule(db *gorm.DB) gin.HandlerFunc {
+    return func(c *gin.Context) {
+        var json Module
+        if err := c.ShouldBindJSON(&json); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return
+        }
+        db.Create(&json)
+        c.JSON(http.StatusOK, gin.H{"success": json})
+    }
 }
